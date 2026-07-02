f98362d3d3092c83dcc9bf52dc1b9153cab8f571
NEWMENG\caoyu <17671739930>
2026-07-02 18:43:59 +0800

    1

---
+++ src/types/templates.ts
@@ -0,0 +1,9 @@
+// 项目模板类型定义
+export interface TemplateItem {
+  name: string; // 展示给用户看的名称
+  value: string; // git 仓库地址
+}
+
+export interface ConfigFileInterface {
+  projectTemplate?: TemplateItem[];
+}
